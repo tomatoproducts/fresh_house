ba51f730fcdcce6fa277baed94055732fc0efcab
Angelina Dorasheva <dorasheva.a@gmail.com>
2018-01-16 19:11:40 +0300

    calc

--- js/calc.js
+++ js/calc.js
@@ -1,33 +1,129 @@
 <script>
-function count() {
-var square = document.getElementById('square').value;
-var price;
-var result;
+    function calcStandart() {
+    var square = document.getElementById("square-standart");
+    var result = document.getElementById("result-standart");
 
-  if(square <= 40 && square <= 50){
-    price == 2000;
-    document.getElementById('result').innerHTML = price;
-    console.log(price);
+    var price = 0;
+    if (parseInt(square.value) <=60) {
+      price +=2000;
+    }
+    if (parseInt(square.value) <70) {
+      if (parseInt(square.value) >=60) {
+        price +=2400;
+      }
+    }
+    if (parseInt(square.value) <80) {
+      if (parseInt(square.value) >=70) {
+        price +=2800;
+      }
+    }
+    if (parseInt(square.value) <90) {
+      if (parseInt(square.value) >=80) {
+        price +=3200;
+      }
+    }
+    if (parseInt(square.value) <100) {
+      if (parseInt(square.value) >=90) {
+        price +=3600;
+      }
+    }
+
+  result.innerHTML = price;
 }
+
+function calcGeneral() {
+    var square = document.getElementById("square-general");
+    var resultGeneral = document.getElementById("result-general");
+
+    var price = 0;
+    if (parseInt(square.value) <=60) {
+      price +=2000;
+    }
+    if (parseInt(square.value) <70) {
+      if (parseInt(square.value) >=60) {
+        price +=2400;
+      }
+    }
+    if (parseInt(square.value) <80) {
+      if (parseInt(square.value) >=70) {
+        price +=2800;
+      }
+    }
+    if (parseInt(square.value) <90) {
+      if (parseInt(square.value) >=80) {
+        price +=3200;
+      }
+    }
+    if (parseInt(square.value) <100) {
+      if (parseInt(square.value) >=90) {
+        price +=3600;
+      }
+    }
+
+  resultGeneral.innerHTML = price;
 }
 
-var btn = document.querySelector(".calc__btn");
-btn.addEventListener("click",count());
+function calcExpress() {
+    var square = document.getElementById("square-express");
+    var resultExpress = document.getElementById("result-express");
 
+    var price = 0;
+    if (parseInt(square.value) <=60) {
+      price +=2000;
+    }
+    if (parseInt(square.value) <70) {
+      if (parseInt(square.value) >=60) {
+        price +=2400;
+      }
+    }
+    if (parseInt(square.value) <80) {
+      if (parseInt(square.value) >=70) {
+        price +=2800;
+      }
+    }
+    if (parseInt(square.value) <90) {
+      if (parseInt(square.value) >=80) {
+        price +=3200;
+      }
+    }
+    if (parseInt(square.value) <100) {
+      if (parseInt(square.value) >=90) {
+        price +=3600;
+      }
+    }
 
-//  var square = document.getElementById('square').value;
-//  var price;
-//  var result;
+  resultExpress.innerHTML = price;
+}
 
-// if(square <= 40 && square <= 50){
-//     price == 2000;
-//     document.getElementById('result').innerHTML = price;
-//     console.log(price);
-// }
-// if(square >=60 && square <=70){
-//     price == 2400;
-//     document.getElementById('result').innerHTML = price;
-//     console.log(price);
-// }
+function calcRemont() {
+    var square = document.getElementById("square-remont");
+    var resultRemont = document.getElementById("result-remont");
 
+    var price = 0;
+    if (parseInt(square.value) <=60) {
+      price +=2000;
+    }
+    if (parseInt(square.value) <70) {
+      if (parseInt(square.value) >=60) {
+        price +=2400;
+      }
+    }
+    if (parseInt(square.value) <80) {
+      if (parseInt(square.value) >=70) {
+        price +=2800;
+      }
+    }
+    if (parseInt(square.value) <90) {
+      if (parseInt(square.value) >=80) {
+        price +=3200;
+      }
+    }
+    if (parseInt(square.value) <100) {
+      if (parseInt(square.value) >=90) {
+        price +=3600;
+      }
+    }
+
+  resultRemont.innerHTML = price;
+}
 </script>
